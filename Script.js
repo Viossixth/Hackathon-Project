dbc15ffdd431e73eb498446d4c7367c06532c846
ViosSixth <risimac@outlook.com>
2025-04-03 10:28:58 +0200

    update

---
+++ Script.js
@@ -0,0 +1,6 @@
+function toggleMenu(){
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("menu-links-open");
+    icon.classList.toggle("hamburger-icon-open");
+}
